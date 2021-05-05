8e11165e1157f20131052cf66d66ae46f1d3b2ba
vectorxz <vectorx2263@hotmail.com>
2021-05-05 23:48:34 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+    },
     extend: {},
   },
   variants: {
