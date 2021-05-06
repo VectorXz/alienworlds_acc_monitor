ddfb9601575c74f5c5d903d3058e20b7c301ed56
vectorxz <vectorx2263@hotmail.com>
2021-05-07 01:20:54 +0700

    edit purge setings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     options: {
-      whitelistPatterns: [/^bg-/, /^text-/]
+      safelist: [/^bg-/, /^text-/]
     }
   },
   darkMode: false, // or 'media' or 'class'
