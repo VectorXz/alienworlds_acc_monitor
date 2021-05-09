a124c8ae13943a884c747bc1bca7d6c7a074d3cc
vectorxz <vectorx2263@hotmail.com>
2021-05-10 00:57:13 +0700

    add wax balance

--- components/AccountInfo.js
+++ components/AccountInfo.js
@@ -10,6 +10,7 @@ export default function AccountInfo(props) {
     const [loading, setLoading] = useState(true)
     const [accInfo, setAccInfo] = useState({})
     const [balance, setBalance] = useState("Loading")
+    const [wax, setWax] = useState("Loading")
     const isInitialMount = useRef(true)
     const isInitialTx = useRef(true)
     const [update, setUpdate] = useState("None")
@@ -54,6 +55,21 @@ export default function AccountInfo(props) {
             setBalance(data[0].slice(0,-4))
         }).catch((err) => {
             setBalance("ERROR")
+        })
+    }
+
+    const getWax = async (user) => {
+        await delay(getRandom(300,2000))
+        return await axios.post('https://chain.wax.io/v1/chain/get_currency_balance',
+        {
+            "code": "eosio.token",
+            "account": user,
+            "symbol": "WAX"
+        }
+        ).then(({data}) => {
+            setWax(data[0].slice(0,-4))
+        }).catch((err) => {
+            setWax("ERROR")
         })
     }
 
@@ -158,6 +174,7 @@ export default function AccountInfo(props) {
             //console.log("CPU Changed!, is now")
             //console.log(accInfo)
             await getBalance(acc)
+            await getWax(acc)
             setLoading(false)
         }
     }, [accInfo])
@@ -215,7 +232,7 @@ export default function AccountInfo(props) {
                         <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
                         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" />
                     </svg>
-                    {balance && <span className="text-lg font-bold text-green-400">{balance} TLM</span>}
+                    {balance && <span className="text-lg font-bold text-green-400">{balance} TLM | {wax} WAX</span>}
                     <a href={'https://wax.atomichub.io/explorer/account/'+acc} className="mx-2 px-2 font-bold text-green-600 bg-green-200 rounded-md" rel="noopener noreferrer" target="_blank">View NFT</a>
                 </div>
             </div>
