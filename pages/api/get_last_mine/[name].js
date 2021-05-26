d0aa3613f28f5c6679175e95be0092dbb2a28fed
vectorxz <vectorx2263@hotmail.com>
2021-05-27 01:24:32 +0700

    [FIX] fix undefined bug2

--- pages/api/get_last_mine/[name].js
+++ pages/api/get_last_mine/[name].js
@@ -18,13 +18,13 @@ export default async (req, res) => {
     { timeout: 15000 }
     ).then((response) => {
         //console.log(data.rows[0]);
-        return res.status(response.status).json(response.data)
+        return res.status(200).json(response.data)
     }).catch((err) => {
         return axios.post('https://api-wax.eosarabia.net/v1/chain/get_table_rows',
         {json: true, code: "m.federation", scope: "m.federation", table: 'miners', lower_bound: name, upper_bound: name},
         { timeout: 15000 }
         ).then((response) => {
-            return response.status(response.status).json(response.data)
+            return response.status(200).json(response.data)
         }).catch((err) => {
             console.log("Error get last mine data")
             console.log(err)
