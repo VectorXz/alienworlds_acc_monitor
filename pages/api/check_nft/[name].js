227119c098a9c8c3b1ad44b8ca015d81505aba40
vectorxz <vectorx2263@hotmail.com>
2021-05-30 18:31:50 +0700

    [FIX] typo

--- pages/api/check_nft/[name].js
+++ pages/api/check_nft/[name].js
@@ -3,7 +3,7 @@ import axios from '../AxiosAPI'
 import delay from 'delay'
 
 export default async (req, res) => {
-    console.log("/get_last_mine called")
+    console.log("/check_nft called")
     const {
         query: { name },
     } = req
