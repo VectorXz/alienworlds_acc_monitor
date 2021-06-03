73f54f8f7badc3cfc36b4380f50230961061f8f2
vectorxz <vectorx2263@hotmail.com>
2021-06-04 00:01:42 +0700

    [IMP] add api

--- backend/get_tag.js
+++ backend/get_tag.js
@@ -11,7 +11,15 @@ const base_api = [
     'https://api.wax.liquidstudios.io',
     'https://wax.eosn.io',
     'https://api.wax.alohaeos.com',
-    'https://wax.greymass.com'
+    'https://wax.greymass.com',
+    'https://wax-bp.wizardsguild.one',
+    'https://apiwax.3dkrender.com',
+    'https://wax.eu.eosamsterdam.net',
+    'https://wax.csx.io',
+    'https://api-wax.eosauthority.com',
+    'https://wax.eoseoul.io',
+    'https://wax.eosphere.io',
+    'https://api.waxeastern.cn'
 ]
 
 function getRandom(min, max) {
