e3cc396a549f0ac127e2f08c2c89cbe0abc8fb6d
vectorxz <vectorx2263@hotmail.com>
2021-06-03 20:16:47 +0700

    [REF] remove old api

--- pages/api/AxiosAPI.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import rateLimit from 'axios-rate-limit';
-
-const http = rateLimit(axios.create(), { maxRequests: 12, perMilliseconds: 1000 })
-console.log("SERVER MAX RPS: "+http.getMaxRPS())
-
-export default http;
