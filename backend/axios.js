0a23288b1fcd2ce1ffb04ee70b1b2470674e11d0
vectorxz <vectorx2263@hotmail.com>
2021-06-04 23:58:27 +0700

    [IMP] apply rate limit with retry

--- backend/axios.js
+++ backend/axios.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 const rateLimit = require('axios-rate-limit')
 const axiosRetry = require('axios-retry')
 
-axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+const http = rateLimit(axios.create(), { maxRequests: 1000, perMilliseconds: 1000, maxRPS: 1000 })
 
-const http = rateLimit(axios.create(), { maxRequests: 200, perMilliseconds: 1000, maxRPS: 200 })
+axiosRetry(http, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
 console.log("Server Axios RPS: "+http.getMaxRPS())
 
